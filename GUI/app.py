ee15f1e5b670150ef274a05803e762af8a4d99c9
Vishesh sharma <sharmavishesh022@gmail.com>
2022-02-22 20:56:08 +0530

    GUI connected via MySQL

--- GUI/app.py
+++ GUI/app.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 from helpers import *
 from kivy.lang import Builder
 from kivymd.app import MDApp
@@ -15,18 +17,45 @@ class LoginScreen(Screen):
 class MenuScreen(Screen):
     pass
 
+class WindowManager(ScreenManager):
+    pass
+
 manager = ScreenManager()
 manager.add_widget(LoginScreen(name='login'))
 manager.add_widget(MenuScreen(name='menu'))
 
 class Welcome(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(screen_helper)
+
+
     def build(self):
         Window.size = (630,630)
         self.theme_cls.theme_style = "Dark"
 
-        screen = Builder.load_string(screen_helper)
+        return self.screen 
 
-        return screen 
+    def connect_to_database(self):
+        username = self.root.get_screen('login').ids.usr.text
+        password = self.root.get_screen('login').ids.pwd.text
+
+        try:
+            db = mysql.connector.connect(
+                host="localhost",
+                user=username,
+                passwd=password,
+                auth_plugin = "mysql_native_password"
+            )		
+            self.root.get_screen('login').ids.btn.text = "[color=#00ffcc]Log in[/color]"
+            self.root.current = 'menu'
+            self.root.get_screen('menu').ids.title.text = f"Welcome {username.capitalize()}!"
+            return db
+        except:
+            self.root.get_screen('login').ids.btn.text = "[color=#ff0000]Log in[/color]"
+            self.root.get_screen('login').ids.pwd.hint_text = "Incorrect Password!"
+            return False
+
 
 if __name__ == '__main__':
     Welcome().run()
--- GUI/helpers.py
+++ GUI/helpers.py
@@ -1,10 +1,14 @@
 screen_helper = """
-ScreenManager:
+WindowManager:
     LoginScreen
     MenuScreen
 
 <LoginScreen>:
     name: 'login'
+    Image:
+        source: "GUI/assets/logo_transparent.png"
+        pos_hint: {"center_x": .2,"center_y": .9}
+
     MDLabel:
         text: "Moment Book"
         halign: "center"
@@ -13,6 +17,7 @@ ScreenManager:
         pos_hint: {'center_x':0.5, 'center_y':0.9}
 
     MDTextField:
+        id: usr
         hint_text: "Enter your username" 
         helper_text: "Your MySQL username"
         helper_text_mode: "on_focus"
@@ -21,26 +26,42 @@ ScreenManager:
         width: 300
         multiline: False
         font_name: "GUI/assets/SecularOne-Regular.ttf"
+
     MDTextField:
+        id: pwd
         hint_text: "Enter your password" 
         pos_hint: {'center_x':0.5, 'center_y':0.4}
         size_hint_x: None
         size_hint_y: None
         width: 300
         height: 400
+        icon_right: "eye-off"
+        password: True
         font_name: "GUI/assets/SecularOne-Regular.ttf"
     MDRectangleFlatButton:
-        text:"Log in"
+        id: btn
+        text:"[color=#00ffcc]Log in[/color]"
         pos_hint: {'center_x':0.5, 'center_y':0.2}
-        on_press: root.manager.current = 'menu'
+        on_press: app.connect_to_database()
         
 
 <MenuScreen>
     name: 'menu'
     MDLabel:
-        text: "Main Menu"
+        id: title
+        text: "What's up?"
         halign: "center"
-        font_style: "H3"
-        pos_hint: {'center_x':0.5, 'center_y':0.9}
+        font_style: "H4"
+        pos_hint: {'center_x':0.4, 'center_y':0.9}
+        font_name: "GUI/assets/SecularOne-Regular.ttf"
+
+    Image:
+        source: "GUI/assets/logo_transparent.png"
+        pos_hint: {"center_x": .1,"center_y": .9}
+
+    MDRectangleFlatButton:
+        text: "Go Back!"
+        pos_hint: {'center_x':0.5, 'center_y':0.2}
+        on_press: root.manager.current = 'login'
 
 """
